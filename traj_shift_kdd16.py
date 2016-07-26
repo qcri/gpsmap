b78836dc4c0fea355c98adc9a8fc850b1dd6013b
sofiane <sabbar@qf.org.qa>
2016-07-27 02:34:46 +0300

    Working code. Problem with isolated points that take so much time.

--- traj_shift_kdd16.py
+++ traj_shift_kdd16.py
@@ -33,12 +33,41 @@ class GpsPoint:
 		return "bt_id:%s, speed:%s, timestamp:%s, lon:%s, lat:%s, angle:%s" % \
 		       (self.btid, self.speed, self.timestamp, self.lon, self.lat, self.angle)
 
+class SamplePoint:
+	def __init__(self, speed=None, lon=None, lat=None, angle=None, weight=None):
+			self.speed = speed
+			self.lon = lon
+			self.lat = lat
+			self.angle = angle
+			self.weight = weight
+
+	def get_coordinates(self):
+		"""
+		return the lon,lat of a gps point
+		:return: tuple (lon, lat)
+		"""
+		return (self.lon, self.lat)
+
+	def __str__(self):
+		return "weight:%s, speed:%s, lon:%s, lat:%s, angle:%s" % \
+		       (self.weight, self.speed, self.lon, self.lat, self.angle)
+
+	def __repr__(self):
+		return "weight:%s, speed:%s, lon:%s, lat:%s, angle:%s" % \
+		       (self.weight, self.speed, self.lon, self.lat, self.angle)
+
+
 class line:
 	def __init__(self, slope=None, intercept=None):
 		self.slope = slope if slope is not None else None
 		self.intercept = intercept if intercept is not None else None
 
 	def perpendecular(self, at_pt=None):
+		if self.slope == 0:
+			# if slope is 0 (horizontal line), the perpendicular is special: x=pt[0].
+			# Thus, I'm givin a special interpretation to slope and intercept here to capture this case.
+			# TODO: find a better solution to handle this case.
+			return line(slope=float('Inf'), intercept=at_pt[0])
 		slope = -1 / self.slope
 		intercept = at_pt[1] - slope * at_pt[0] if at_pt is not None else 0
 		return line(slope=slope, intercept=intercept)
@@ -53,7 +82,7 @@ class line:
 	def __repr__(self):
 		return 'y = %sx + %s' % (self.slope, self.intercept)
 
-def project_point_into_line(pt, parallel_line, perpendecular_line):
+def project_point_into_line(pt, parallel_line, perpendicular_line):
 	"""
     This functions finds the projected point (ppt) of a point (pt) on a line (line). It computed the intersection
     between line at the perpondecular line to it that goes through pt. 
@@ -61,13 +90,17 @@ def project_point_into_line(pt, parallel_line, perpendecular_line):
     :param line: line defined with (intercept, slope)
     :returns: intersection point (x, y)
     """
+
+	if perpendicular_line.slope == float('Inf'):
+		# special case: projecting on a vertical line, y=point's y, x=intercept that has special meaning here.
+		return (perpendicular_line.intercept, pt[1])
 	# 1. find the line that is parallel to parallel line that goes thu pt.
 	intercept = pt[1] - parallel_line.slope * pt[0]
 	#line(parallel_line.slope, intercept).plot(color='green', width=1)
 
 	# 2. find intersection point between pt_line and perpendicular_line
-	X = np.array([[1, - parallel_line.slope], [1, -perpendecular_line.slope]])
-	Y = np.array([intercept, perpendecular_line.intercept])
+	X = np.array([[1, - parallel_line.slope], [1, -perpendicular_line.slope]])
+	Y = np.array([intercept, perpendicular_line.intercept])
 	yx = np.linalg.solve(X, Y)
 	return (yx[1], yx[0])
 
@@ -115,7 +148,10 @@ def haversine(pt1, pt2):
 	return km * 1000
 
 
-def find_sample(rand_pt=None, neighbors=None):
+def find_center(rand_pt=None, neighbors=None, draw_output=False):
+	if len(neighbors) < 2:
+		return SamplePoint(lon=rand_pt.lon, lat=rand_pt.lat, speed=rand_pt.speed, angle=rand_pt.angle, weight=len(neighbors))
+
 	in_pt = rand_pt.get_coordinates()
 	angle = rand_pt.angle
 
@@ -123,8 +159,8 @@ def find_sample(rand_pt=None, neighbors=None):
 	eq_heading = line_of_gps_point(in_pt, angle)
 	# equation of the perpendicular line to the point's heading line:
 	eq_perpend = eq_heading.perpendecular(at_pt=in_pt)
-	print 'heading eq:', eq_heading
-	print 'perpendecular eq:', eq_perpend
+	#print 'heading eq:', eq_heading
+	#print 'perpendecular eq:', eq_perpend
 
 	# Project all points into the perpendicular line
 	projected_points = list()
@@ -153,12 +189,14 @@ def find_sample(rand_pt=None, neighbors=None):
 	segment_length = max([haversine(neighbors[i].get_coordinates(), neighbors[j].get_coordinates())\
 	                      for i in range(len(neighbors)) for j in range(i+1, len(neighbors))])
 	nb_bins = int(ceil(segment_length/LANE_WIDTH))
-	print 'nb_bins: ', nb_bins
+	# Handle the case where nb_bins = 0 / this happens if all points are superposed (collision)
+	if nb_bins  == 0:
+		nb_bins = 1
 	histog = np.histogram(axis, bins=nb_bins, density=True)
 	densities, bin_limits = histog[0], histog[1]
 
 	# Find the relevant/central bin:
-	common_denominator = sum([distance_heading_speed(rand_pt, nei) for nei in neighbors])
+	#common_denominator = sum([distance_heading_speed(rand_pt, nei) for nei in neighbors])
 	bin_votes = []
 	for i, (bmin, bmax) in enumerate(zip(bin_limits[:-1], bin_limits[1:])):
 		# find neighbors that are inside the bin limits
@@ -167,9 +205,10 @@ def find_sample(rand_pt=None, neighbors=None):
 			if (axis_label == 'x-axis' and nei.lon >= bmin and nei.lon < bmax) or \
 					(axis_label == 'y-axis' and nei.lat >= bmin and nei.lat < bmax):
 				votes += distance_heading_speed(rand_pt, nei)
-		bin_votes.append(votes/common_denominator)
+		bin_votes.append(votes)
+		#bin_votes.append(votes/common_denominator)
 	max_density_bin = np.argmax(bin_votes)
-	print 'max bin: %s, bins votes: %s' % (max_density_bin, bin_votes)
+	print 'nb bins: %s, max bin: %s' % (nb_bins, max_density_bin)
 	#max_density_bin = np.argmax(densities)
 	marker = (bin_limits[max_density_bin] + bin_limits[max_density_bin + 1]) / 2
 	# find the other component of the sample point: parallel to x-axis or y-axis
@@ -180,9 +219,13 @@ def find_sample(rand_pt=None, neighbors=None):
 		Sy = marker
 		Sx = (Sy - eq_perpend.intercept) / eq_perpend.slope
 
+	if draw_output == False:
+		return SamplePoint(lon=Sx, lat=Sy, speed=rand_pt.speed, angle=rand_pt.angle, weight=len(neighbors))
+
 	# ---------------------
 	# plotting the results |
 	# ---------------------
+
 	eq_heading.plot(color='blue', pt=in_pt)
 	eq_perpend.plot(color='red', pt=in_pt)
 	plt.scatter(in_pt[0], in_pt[1], marker='8', s=50, color='r')
@@ -200,8 +243,7 @@ def find_sample(rand_pt=None, neighbors=None):
 	# Get x/y-axis in the same aspect
 	plt.gca().set_aspect('equal', adjustable='box')
 	plt.show()
-
-	return (Sx, Sy)
+	return SamplePoint(lon=Sx, lat=Sy, speed=rand_pt.speed, angle=rand_pt.angle, weight=len(neighbors))
 
 def retrieve_neighbors(in_pt, points_tree, radius=5):
 	"""
@@ -238,24 +280,60 @@ def load_data(fname='data/gps_data/gps_points.csv'):
 	points_tree = cKDTree(raw_points)
 	return np.array(data_points), np.array(raw_points), points_tree
 
+def summarize_gps_cloud():
+	INPUT_FILE_NAME = 'data/gps_data/gps_points_07-11.csv'
+	RADIUS = 0.0001
+	data_points, raw_points, points_tree = load_data(fname=INPUT_FILE_NAME)
+	print 'nb points:',  len(data_points), 'points example:', data_points[0], raw_points[0]
+	samples = list()
+	removed_points = set()
+	copy_data_points = np.copy(data_points)
+	available_point_indexes = np.arange(0, len(data_points))
+	while len(available_point_indexes) > 0:
+		print 'THERE ARE %s points, removed %s points' % (len(available_point_indexes), len(removed_points))
+		rand_index = random.sample(available_point_indexes, 1)[0]
+		rand_pt = copy_data_points[rand_index]
+
+		# Find all neighbors in the given radius: RADIUS
+		neighbor_indexes = retrieve_neighbors(in_pt=rand_pt.get_coordinates(), points_tree=points_tree, radius=RADIUS)
+		remaining_neighbor_indexes = list(set(neighbor_indexes) - removed_points)
+		neighbors = copy_data_points[remaining_neighbor_indexes]
+
+		print 'NB Neighbors: %s' % len(remaining_neighbor_indexes), ' point: ', rand_pt.get_coordinates()
+		# call find center method
+		sample_pt = find_center(rand_pt=rand_pt, neighbors=neighbors, draw_output=False)
+		samples.append(sample_pt)
+
+		# Remove elements
+		available_point_indexes = np.delete(available_point_indexes, list(remaining_neighbor_indexes))
+		removed_points = removed_points.union(remaining_neighbor_indexes)
+		if len(removed_points) > 100000:
+			break
+
+	print 'NB SAMPLES: %s' % len(samples)
+	for s in samples:
+		if s.lon > 50:
+			plt.scatter(s.lon, s.lat)
+
+	# Get x/y-axis in the same aspect
+	plt.gca().set_aspect('equal', adjustable='box')
+	plt.show()
 
 if __name__ == "__main__":
-	INPUT_FILE_NAME = 'data/gps_data/gps_points_07-11.csv'
-	data_points, raw_points, points_tree = load_data(fname=INPUT_FILE_NAME)
-	print 'nb points:',  len(data_points), len(raw_points)
-	print 'points example:', data_points[0:3], raw_points[0:3]
-
-	# input: point and angle:
-	rand_pt = random.sample(data_points, 1)[0]
-	pt_coordinates = rand_pt.get_coordinates()
-	angle = rand_pt.angle
-
-	# prepare some input neighbors
-	# neighbors = np.random.randint(10, size=(100, 2))
-
-	neighbor_indexes = retrieve_neighbors(in_pt=pt_coordinates, points_tree=points_tree, radius=0.0001)
-	print 'pt:%s, angle:%s,  has %s neighbors' % (pt_coordinates, angle, len(neighbor_indexes))
-	neighbors = data_points[neighbor_indexes]
-
-	# call find sample method
-	sample_pt = find_sample(rand_pt=rand_pt, neighbors=neighbors)
+	summarize_gps_cloud()
+	# INPUT_FILE_NAME = 'data/gps_data/gps_points_07-11.csv'
+	# RADIUS = 0.0001
+	# data_points, raw_points, points_tree = load_data(fname=INPUT_FILE_NAME)
+	# print 'nb points:',  len(data_points), 'points example:', data_points[0], raw_points[0]
+	#
+	# # input: point and angle:
+	# rand_pt = random.sample(data_points, 1)[0]
+	#
+	# # Find all neighbors in the given radius radius
+	# # neighbors = np.random.randint(10, size=(100, 2))
+	# neighbor_indexes = retrieve_neighbors(in_pt=rand_pt.get_coordinates(), points_tree=points_tree, radius=RADIUS)
+	# print 'pt:%s, angle:%s,  has %s neighbors' % (rand_pt.get_coordinates(), rand_pt.angle, len(neighbor_indexes))
+	# neighbors = data_points[neighbor_indexes]
+	#
+	# # call find center method
+	# sample_pt = find_center(rand_pt=rand_pt, neighbors=neighbors)
