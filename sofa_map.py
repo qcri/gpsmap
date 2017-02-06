6f629be4add90c56f578007242753e1f5c55ee8b
sofiane <sofiane.abbar@gmail.com>
2017-02-06 16:58:20 +0300

    fixing some problem with the streaming graph spanner

--- sofa_map.py
+++ sofa_map.py
@@ -33,10 +33,11 @@ class Cluster:
 from common import *
 import math
 
-RADIUS_METER = 25
+RADIUS_METER = 20
 RADIUS_DEGREE = RADIUS_METER * 10e-6
 HEADING_ANGLE_TOLERANCE = 10
 MAX_PATH_LEN = 5
+MAX_PATH_DISTANCE_FACTOR = 1.3
 
 INPUT_FILE_NAME = 'data/gps_data/gps_points_07-11.csv'
 FILE_CODE = 'gps_points_07-11'
@@ -50,22 +51,94 @@ cluster_kdtree = None
 
 roadnet = nx.DiGraph()
 
-def satisfy_path_condition(G, s, t, max_path_len):
+
+def k_reachable_from(g, k):
+	"""
+	Build a dictionary that returns for each node, all the nodes from which it can be reached within radius k.
+	:param g:
+	:param k:
+	:return:
+	"""
+	nodes = g.nodes()
+	k_reach_from = defaultdict(set)
+	for node in nodes:
+		sub_g = nx.ego_graph(g, node, radius=k)
+		for n in sub_g.nodes():
+			k_reach_from[n].add(node)
+	return k_reach_from
+
+
+def update_k_reachability(source, roadnet, k, k_reach):
+	"""
+	In order to update the k_reachability of only those nodes that will be impacted by the creation of a new
+	edge: s --> t.
+	:param s: source node of the new edge
+	:param t: target node of the new edge
+	:param g: the road graph
+	:param k: reachability radius
+	:param k_reach: dictionary of k_reachability to be updated
+	:return: updated k_reachability
+	"""
+	inv_g = roadnet.reverse()
+	inv_reachability = nx.ego_graph(inv_g, source, radius=k)
+	for node in inv_reachability:
+		k_reach[node] = nx.ego_graph(roadnet, node, radius=k).nodes()
+	return k_reach
+
+def k_reachability(g, k):
+	"""
+	Build a dictionary in which we return for each node, all nodes that we can reach within k hops.
+	:param g: graph
+	:param k: number of hops
+	:return: dictionary
+	"""
+	nodes = g.nodes()
+	k_reach = defaultdict(list)
+	for node in nodes:
+		sub_g = nx.ego_graph(g, node, radius=k)
+		k_reach[node] = sub_g.nodes()
+	return k_reach
+
+
+
+def satisfy_path_condition(s, t, k_reach):
 	"""
 	I need to check if there's a path of length max_path_len or less. If so, return false.
 	If true, then create an edge.
 	To create an edge there shouldn't exist a path of length < max_path_len
-	:param G:
 	:param s:
 	:param t:
-	:param max_path_len:
+	:param k_reach:
 	:return:
 	"""
-	path_lens = [len(path) for path in nx.all_simple_paths(G, s, t) if len(path) < max_path_len and len(path) > 1]
-	print '\n len path: %s' % len(list(nx.all_simple_paths(G, s, t)))
-	if len(path_lens) == 0:
-		return True
-	return False
+	if s == -1 or t == -1 or s == t or (s in k_reach.keys() and t in k_reach[s]):
+		return False
+	return True
+
+def satisfy_path_condition_distance(s, t, g, clusters, max_length_distance_factor):
+	"""
+	return False if there's a path of length max length, True otherwise
+	:param s:
+	:param t:
+	:param k_reach:
+	:return:
+	"""
+
+	if s == -1 or t == -1:
+		return False
+	edge_distance = geopy.distance.distance(geopy.Point(clusters[s].get_coordinates()),\
+	                                        geopy.Point(clusters[t].get_coordinates())).meters
+
+	print 'source, target;', s, t
+	path = nx.shortest_path(g, source=s, target=t)
+	path_length_meters = 0
+	for i in range(1, len(path)):
+		path_length_meters += geopy.distance.distance(geopy.Point(clusters[path[i - 1]].get_coordinates()),\
+	                                        geopy.Point(clusters[path[i]].get_coordinates())).meters
+
+	if path_length_meters <= max_length_distance_factor * edge_distance:
+		return False
+	return True
 
 for i, trajectory in enumerate(trajectories[:200]):
 	sys.stdout.write('\rprocessing trajectory: %s' % i)
@@ -73,6 +146,9 @@ for i, trajectory in enumerate(trajectories[:200]):
 
 	prev_cluster = -1
 	current_cluster = -1
+	k_path_length = 5
+	k_reach = defaultdict(list)
+	first_edge = True
 	for point in trajectory:
 		if len(clusters) == 0:
 			# create a new cluster
@@ -105,10 +181,15 @@ for i, trajectory in enumerate(trajectories[:200]):
 			current_cluster = closest_cluster_indx
 
 		# create an edge between two consecutive clusters.
-		if current_cluster != -1 and prev_cluster != -1 and prev_cluster != current_cluster and\
-				(not current_cluster in roadnet.neighbors(prev_cluster)) and (satisfy_path_condition(roadnet, prev_cluster, current_cluster, MAX_PATH_LEN)):
+
+		if satisfy_path_condition_distance(prev_cluster, current_cluster, roadnet, clusters, MAX_PATH_DISTANCE_FACTOR):
 				roadnet.add_edge(prev_cluster, current_cluster)
 				prev_cluster = current_cluster
+				# if first_edge:
+				# 	k_reach = k_reachability(roadnet, k_path_length)
+				# 	first_edge = False
+				# # else:
+				# 	# k_reach = update_k_reachability(prev_cluster, roadnet, k_path_length, k_reach)
 		elif prev_cluster == -1 and current_cluster != -1:
 			prev_cluster = current_cluster
 
